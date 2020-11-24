bbd2f9b08d45eec2051703d6df5b3aee928c7324
Junmin De Real <junmindereal@gmail.com>
2020-11-24 12:00:12 +0800

    added tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
