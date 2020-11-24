f6ef87f81db816a399362712c13a18aee872c6f6
Junmin De Real <junmindereal@gmail.com>
2020-11-24 13:24:41 +0800

    updated tailwindcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/*.js', './components/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
